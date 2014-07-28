ef7c37bf840fb126734fdff50789f17c84fcf562
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-07-28 16:57:59 +0100

    Added first page link and test

--- server.rb
+++ server.rb
@@ -16,10 +16,16 @@ DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
 	
-  get '/' do
-    'Hello BookmarkManager!'
-  end
+  # get '/' do
+  #   'Hello BookmarkManager!'
+  # end
+
+	get '/' do
+  	@links = Link.all
+  	erb :index
+	end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
